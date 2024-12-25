981ecc6557f91ff96f62a9fd7d49b761888f7f5d
Sajjin <sajjinnijjar@gmail.com>
2024-12-24 21:56:53 -0800

    Update VideoSourceSelector to change video source URLs for improved reliability

--- src/components/VideoSourceSelector.tsx
+++ src/components/VideoSourceSelector.tsx
@@ -10,8 +10,8 @@ export const VIDEO_SOURCES: VideoSource[] = [
     name: 'Source 1 (better quality but may hitch)',
     getUrl: (imdbId, season, episode) =>
       season && episode
-        ? `https://vidsrc.to/embed/tv/${imdbId}/${season}/${episode}`
-        : `https://vidsrc.to/embed/movie/${imdbId}`
+        ? `https://multiembed.mov/?video_id=${imdbId}&s=${season}&e=${episode}`
+        : `https://multiembed.mov/?video_id=${imdbId}`
   },
   {
     name: 'Source 2 (faster but lower quality)',
