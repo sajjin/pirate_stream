8c80f90a04df2d5351172d5c0023ed0d5b3429a5
Sajjin <sajjinnijjar@gmail.com>
2026-03-16 01:47:22 -0700

    Remove AWS Amplify configuration from App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AuthPage from './pages/AuthPages';
 import HomePage from './pages/Home';
-import { Amplify } from 'aws-amplify';
 import './config';
-import config from './amplifyconfiguration.json';
 import { authPersistence } from './auth/authPersistence';
-
-Amplify.configure(config);
 
 
 const App: React.FC = () => {
